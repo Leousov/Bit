656f9507aef05749541338cf7cd56e60d2af62af
leousovs@gmail.com <leousovs@gmail.com>
2023-04-20 23:16:14 +0500

    Нерабочая авторизация

--- Bit/Program.cs
+++ Bit/Program.cs
@@ -1,5 +1,13 @@
 using Bit;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +28,28 @@ builder.Services.AddSwaggerGen();
 //                      });
 //});
 builder.Services.AddCors();
+builder.Services.AddAuthentication("Bearer")  
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // указывает, будет ли валидироваться издатель при валидации токена
+            ValidateIssuer = true,
+            // строка, представляющая издателя
+            ValidIssuer = AuthOptions.ISSUER,
+            // будет ли валидироваться потребитель токена
+            ValidateAudience = true,
+            // установка потребителя токена
+            ValidAudience = AuthOptions.AUDIENCE,
+            // будет ли валидироваться время существования
+            ValidateLifetime = true,
+            // установка ключа безопасности
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            // валидация ключа безопасности
+            ValidateIssuerSigningKey = true,
+        };
+    });
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,6 +59,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseCors(builder => { builder.AllowAnyOrigin(); builder.AllowAnyHeader(); builder.AllowAnyMethod(); });
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 //app.MapControllerRoute(
@@ -40,7 +71,64 @@ app.MapControllers();
 //    (string controller, string action, string? id) =>
 //        action);
 
-app.MapGet("/Data/Read/", (ApplicationContext db) => db.Data.ToList());
-app.MapPost("/Data/Write/", (Data d, ApplicationContext db) => { db.Data.Add(d); db.SaveChanges(); return d; });
+app.MapGet("/Data/Read/", [Authorize](ApplicationContext db) => {
+    try
+    {
+        db.Data.ToList();
+    }
+    catch
+    {
+        Results.StatusCode(400);
+    }
+});
+app.MapPost("/Data/Write/", [Authorize](Data d, ApplicationContext db) =>
+{
+    try {
+        db.Data.Add(d);
+        db.SaveChanges();
+        return d;
+    }
+    catch
+    {
+        Results.StatusCode(400);
+        return null;
+    }
+     });
+app.MapPost("/login/", (Users loginData, ApplicationContext db) =>
+{
+    bool q = true;
+    Users? person = new Users();
+    foreach (var user in db.Users.ToList())
+    {
+        if (loginData.Login == user.Login && loginData.Password == user.Password)
+        {
+            person = user;
+            q = false;
+        }
+    }
+    if (q) return Results.Unauthorized();
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, person.Login) };
+    var jwt = new JwtSecurityToken(
+            issuer: AuthOptions.ISSUER,
+            audience: AuthOptions.AUDIENCE,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+    var response = new
+    {
+        access_token = encodedJwt,
+        username = person.Login
+    };
 
+    return Results.Json(response);
+});
 app.Run();
+public class AuthOptions
+{
+    public const string ISSUER = "MyAuthServer"; // издатель токена
+    public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+    const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+}
